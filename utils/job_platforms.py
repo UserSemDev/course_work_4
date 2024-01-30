822c701f9cc3c03249a08e2df0a502eb2ca2123b
Sem <semseveryanin@gmail.com>
2024-01-30 23:06:21 +0300

    added docstrings and fix methods

--- utils/job_platforms.py
+++ utils/job_platforms.py
@@ -1,22 +1,34 @@
+import random
+import time
 from abc import ABC, abstractmethod
 import requests
 from utils.exceptions import ResponseError
-import random
-import time
 
 
 class JobVacancyAPI(ABC):
+    """Абстрактный класс пря поиска вакансий через API"""
 
     @abstractmethod
     def get_vacancies(self, **kwargs):
+        """Абстрактный метод получения вакансий"""
+        pass
+
+    @staticmethod
+    def correct_query(user_params: dict) -> dict:
+        pass
+
+    @staticmethod
+    def get_params_vacancy(job_item: dict, index: int) -> dict:
         pass
 
 
 class HeadHunterAPI(JobVacancyAPI):
+    """Класс для рабы с API HeadHunter"""
 
     url = 'https://api.hh.ru/vacancies/'
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Инициализация параметров для запроса к API"""
         self.params = {
             'text': '',
             'only_with_salary': False,
@@ -30,45 +42,51 @@ class HeadHunterAPI(JobVacancyAPI):
                           "Chrome/120.0.0.0 Safari/537.36"
         }
 
-    def get_vacancies(self, **kwargs):
+    def get_vacancies(self, **kwargs: dict) -> None:
+        """
+        Метод для получения вакансий с API HeadHunter
+        :param kwargs: словарь полученный от пользователя с параметрами для запроса
+        :return: None
+        """
         data = self.correct_query(kwargs)
         self.params['text'] = data['search_query']
         self.params['only_with_salary'] = data['salary']
         if data.get('experience'):
             self.params['experience'] = data['experience']
-        try:
-            response = requests.get(self.url, params=self.params, headers=self.headers)
-            if response.status_code == 200:
-                resp = response.json()
-                found = resp['found']
-                pages = resp['pages']
-                index = 0
-                for page_num in range(pages):
+        index = 0
+        while True:
+            try:
+                response = requests.get(self.url, params=self.params, headers=self.headers)
+                if response.status_code == 200:
+                    resp = response.json()
+                    items = resp['items']
+                    found = resp['found']
                     page = resp['page']
-                    for job in resp['items']:
+                    pages = resp['pages']
+                    for item in items:
                         index += 1
-                        job_vacancy = self.get_params_vacancy(job, index)
+                        job_vacancy = self.get_params_vacancy(item, index)
                         print(job_vacancy)
-                    if page < pages:
-                        self.params['page'] = self.params.get('page') + 1
-                        resp = self.get_page_vacancy()
+                    if page == pages - 1:
+                        break
+                    self.params['page'] = self.params.get('page') + 1
+                    random_time = random.uniform(0.2, 0.4)
+                    time.sleep(random_time)
+                else:
+                    raise ResponseError(f'Ошибка запроса, статус код: [{response.status_code}]')
+            except ResponseError as ex:
+                print(ex)
+                exit()
 
-                print(f'Всего вакансий: {found}')
-
-        except ResponseError as ex:
-            print(ex)
-
-    def get_page_vacancy(self):
-        random_time = random.uniform(0.2, 0.4)
-        time.sleep(random_time)
-        response = requests.get(self.url, params=self.params, headers=self.headers)
-        if response.status_code == 200:
-            data = response.json()
-            return data
+        print(f'Всего вакансий: {found}')
 
     @staticmethod
-    def correct_query(value):
-
+    def correct_query(user_params: dict) -> dict:
+        """
+        Метод обработки полученных от пользователя данных для обращения к API
+        :param user_params: словарь с параметрами полученный от пользователя
+        :return: откорректированный словарь параметров для запроса к API
+        """
         dict_experience = {
             range(0, 1): 'noExperience',
             range(1, 3): 'between1And3',
@@ -76,26 +94,32 @@ class HeadHunterAPI(JobVacancyAPI):
             range(6, 100): 'moreThan6'
         }
 
-        experience = value['experience']
+        experience = user_params['experience']
         if experience.strip().isdigit():
             for key in dict_experience.keys():
                 if int(experience) in key:
-                    value['experience'] = dict_experience[key]
+                    user_params['experience'] = dict_experience[key]
         else:
-            del value['experience']
+            del user_params['experience']
 
-        salary = value['salary']
+        salary = user_params['salary']
         if salary.isdigit():
             salary = int(salary)
             data = True if salary == 1 else False
         else:
             data = False
-        value['salary'] = data
+        user_params['salary'] = data
 
-        return value
+        return user_params
 
     @staticmethod
-    def get_params_vacancy(job_item, index):
+    def get_params_vacancy(job_item: dict, index: int) -> dict:
+        """
+        Метод получающий параметры вакансии и возвращающий словарь
+        :param job_item: json словарь полученный от API с вакансией
+        :param index: счетчик вакансий
+        :return: возвращает словарь с вакансией
+        """
         name = job_item['name']
         if job_item['salary']:
             salary = {'from': job_item['salary']['from'],
@@ -103,16 +127,22 @@ class HeadHunterAPI(JobVacancyAPI):
                       'currency': job_item['salary']['currency']}
         else:
             salary = None
-        experience = job_item['experience']['name']
-        alternate_url = job_item['alternate_url']
+        experience = job_item.get('experience').get('name')
+        employer = {
+            'name': job_item.get('employer').get('name'),
+            'alternate_url': job_item.get('employer').get('alternate_url')
+        }
+        alternate_url = job_item.get('alternate_url')
         data = {'id': index,
                 'name': name,
                 'salary': salary,
                 'experience': experience,
+                'employer': employer,
                 'url_vacancy': alternate_url}
         return data
 
 
 class SuperJobAPI(JobVacancyAPI):
     def get_vacancies(self, **kwargs):
+        """Метод для получения вакансий с API SuperJob"""
         pass
