0ab13271af2b959370f047ed94e946ade06936c9
Sem <semseveryanin@gmail.com>
2024-02-06 01:49:05 +0300

    added path

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(__file__)
-VACANCY_JSON_PATH = os.path.join(ROOT_DIR, 'data', 'job_vacancy.json')
+VACANCY_ALL_JSON_PATH = os.path.join(ROOT_DIR, 'data', 'all_job_vacancies.json')
+VACANCY_FILTER_JSON_PATH = os.path.join(ROOT_DIR, 'data', 'filter_job_vacancies.json')
