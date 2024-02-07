031777a8a5aff64b010d36b435f1c442341c7db1
Sem <semseveryanin@gmail.com>
2024-02-08 01:16:05 +0300

    add magic method

--- src/vacancy.py
+++ src/vacancy.py
@@ -14,17 +14,19 @@ class Vacancy(MixinExchanges):
         else:
             self.from_salary = self.salary.get('from')
             self.to_salary = self.salary.get('to')
-        self.convert_salary()
+        if self.salary['currency'] != 'руб.':
+            self.convert_salary()
         self.experience = kwargs['experience']
         self.description = kwargs['description']
+        self.area = kwargs['area']
         self.employer = kwargs['employer']
         self.url_vacancy = kwargs['url_vacancy']
         self.platform = kwargs['platform']
         self.__value = 0
 
-    def __repr__(self) -> str:
+    def __str__(self) -> str:
         """
-        Магический метод отображения вакансии
+        Магический метод отображения вакансии для пользователя
         :return: возвращает строку с вакансией
         """
         if self.salary:
@@ -38,7 +40,18 @@ class Vacancy(MixinExchanges):
                 salary = "не указана"
         else:
             salary = "не указана"
-        return f"Вакансия: {self.name} | Зарплата: {salary} | Опыт работы: {self.experience} | URL вакансии: {self.url_vacancy}"
+        return (f"Вакансия: {self.name} | Зарплата: {salary} | Опыт работы: {self.experience} | "
+                f"Город: {self.area} | URL вакансии: {self.url_vacancy}")
+
+    def __repr__(self) -> str:
+        """
+        Магический метод отображения вакансии для разработчика
+        :return: возвращает строку с вакансией
+        """
+        data = (f"Экземпляр класса: {self.__class__.__name__}\n"
+                f"Аргументы: {self.id=}, {self.name=}, {self.salary=}, {self.experience=}, {self.description=}, "
+                f"{self.area=}, {self.employer=}, {self.url_vacancy=}, {self.platform=}")
+        return data
 
     @property
     def value(self) -> int:
@@ -68,33 +81,34 @@ class Vacancy(MixinExchanges):
         :return: None
         """
         rates = MixinExchanges.get_exchanges_rates()
-        if self.salary:
-            cur_s = self.salary['currency']
-            if cur_s != 'RUR':
-                from_s = self.from_salary
-                to_s = self.to_salary
-                if from_s:
-                    for rate in rates:
-                        if cur_s in rate['code']:
-                            self.from_salary = round(from_s / rate['units'] * rate['course'])
-                            self.salary['from'] = self.from_salary
-                else:
-                    self.from_salary = 0
-                    self.salary['from'] = 0
-                if to_s:
-                    for rate in rates:
-                        if cur_s in rate['code']:
-                            self.to_salary = round(to_s / rate['units'] * rate['course'])
-                            self.salary['to'] = self.to_salary
-                else:
-                    self.to_salary = 0
-                    self.salary['to'] = 0
+        if not (rates == -1):
+            if self.salary:
+                cur_s = self.salary['currency']
+                if cur_s != 'RUR':
+                    from_s = self.from_salary
+                    to_s = self.to_salary
+                    if from_s:
+                        for rate in rates:
+                            if cur_s in rate['code']:
+                                self.from_salary = round(from_s / rate['units'] * rate['course'])
+                                self.salary['from'] = self.from_salary
+                    else:
+                        self.from_salary = 0
+                        self.salary['from'] = 0
+                    if to_s:
+                        for rate in rates:
+                            if cur_s in rate['code']:
+                                self.to_salary = round(to_s / rate['units'] * rate['course'])
+                                self.salary['to'] = self.to_salary
+                    else:
+                        self.to_salary = 0
+                        self.salary['to'] = 0
 
-            self.salary['currency'] = 'руб.'
+                self.salary['currency'] = 'руб.'
 
-        else:
-            self.from_salary = 0
-            self.to_salary = 0
+            else:
+                self.from_salary = 0
+                self.to_salary = 0
 
     def correct_info_vacancy(self) -> dict:
         """
@@ -107,6 +121,7 @@ class Vacancy(MixinExchanges):
             'salary': self.salary,
             'experience': self.experience,
             'description': self.description,
+            'area': self.area,
             'employer': self.employer,
             'url_vacancy': self.url_vacancy,
             'platform': self.platform
